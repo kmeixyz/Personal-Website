d7a89fb7d1ee4b94d47bf7c5eeb869fe563ae6e3
Kevin Mei <kevinmei@dhcp-10-105-146-61.wireless.northwestern.private>
2025-05-08 00:11:06 -0500

    testing name and aboutme

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,9 @@ import Image from "next/image";
 export default function Home() {
   return (
     <>
-     <div><h1>Kevin Mei</h1></div>
-     <div><h2>About Me</h2></div>
-     <div><h3>Okay</h3></div>
+     <div><h1 className='text-blue-500 text-4xl font-bold text-center'>Kevin Mei</h1></div>
+     <div><h2 className='text=[#e0115f] text-8xl font-bold text-center'>About Me</h2></div>
+     <div><h3 className='text=red-500 text-5xl font-bold text-center'>I enjoy making things.</h3></div>
     </>
   )
 }
